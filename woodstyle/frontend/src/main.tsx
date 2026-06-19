8cc9c6aed0b098ddf0bfd15b3a64f49a2b196907
Igor Maisner <fgydvg@gmail.com>
2026-06-20 01:52:35 +0500

    feat: set dynamic basename for BrowserRouter using environment variable

--- woodstyle/frontend/src/main.tsx
+++ woodstyle/frontend/src/main.tsx
@@ -16,9 +16,11 @@ import App from './app/App'
 import { AppProviders } from './app/providers'
 import './styles/global.css'
 
+const routerBaseName = import.meta.env.BASE_URL.replace(/\/$/, '') || '/'
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={routerBaseName}>
       <AppProviders>
         <App />
       </AppProviders>
